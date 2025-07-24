7b3f7e4be9ae16177fb173378da8f516db39b0b1
iamaber <abirislam1892@gmail.com>
2025-07-24 13:20:19 +0600

    Implement sentiment analysis functionality

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,8 +1,6 @@
 import feedparser
 
-def fetch_headlines(url="https://feedparser.readthedocs.io/en/latest/examples/atom10.xml"):
+def fetch_headlines(url="http://rss.cnn.com/rss/cnn_topstories.rss"):
     feed = feedparser.parse(url)
     headlines = [entry['title'] for entry in feed['entries']]
     return headlines
-
-print(fetch_headlines())
