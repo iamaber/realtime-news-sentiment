2b86222d9f006b506991405a4c4750b0e5855e60
iamaber <abirislam1892@gmail.com>
2025-07-25 18:35:04 +0600

    Refactor fetch_headlines function to accept multiple RSS feed URLs and return aggregated headlines

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,6 +1,20 @@
 import feedparser
 
-def fetch_headlines(url="http://rss.cnn.com/rss/cnn_topstories.rss"):
-    feed = feedparser.parse(url)
-    headlines = [entry['title'] for entry in feed['entries']]
-    return headlines
+def fetch_headlines(urls=None):
+    urls = [
+        "https://news.google.com/rss",
+        "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+        "https://www.bbc.co.uk/feeds/rss/news/world/rss.xml"
+        "https://moxie.foxnews.com/google-publisher/latest.xml",
+        "https://www.cnbc.com/id/100003114/device/rss/rss.html",
+        "https://www.aljazeera.com/xml/rss/all.xml", 
+        "https://news.yahoo.com/rss/mostviewed",
+        "http://rss.cnn.com/rss/edition.rss"
+    ]
+    all_headlines = []
+    for url in urls:
+        feed = feedparser.parse(url)
+        headlines = [entry['title'] for entry in feed.get('entries', [])]
+        all_headlines.extend(headlines)
+    
+    return all_headlines
