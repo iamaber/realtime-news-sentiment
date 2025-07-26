aa0ea0865b76f0030d41ec29a6cb4f983a9bc27e
iamaber <abirislam1892@gmail.com>
2025-07-26 13:01:47 +0600

    Fix API endpoint URL in data fetching logic

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from collections import Counter
 st.set_page_config(page_title="News Sentiment Dashboard")
 
 if "data" not in st.session_state:
-    response = requests.get("http://localhost:8000/")
+    response = requests.get("http://fastapi:8000/")
     if response.status_code == 200:
         st.session_state.data = response.json()
     else:
